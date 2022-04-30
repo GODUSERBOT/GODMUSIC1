f682b94f629e0ea806ff4b4494f514b3a5a6e0f0
GOD INVISIBLE <104170029+GODUSERBOT@users.noreply.github.com>
2022-04-30 21:40:31 +0530

    Update __init__.py

--- Gods/converter/__init__.py
+++ Gods/converter/__init__.py
@@ -3,6 +3,6 @@ from os import listdir, mkdir
 if "raw_files" not in listdir():
     mkdir("raw_files")
 
-from Rockerz.converter.converter import convert
+from Gods.converter.converter import convert
 
 __all__ = ["convert"]
