20aab46094bfbd5a0e625c7737c6015e8ac07792
GOD INVISIBLE <104170029+GODUSERBOT@users.noreply.github.com>
2022-04-30 22:08:23 +0530

    Update admins.py

--- Godmusic/admins.py
+++ Godmusic/admins.py
@@ -5,7 +5,7 @@ from asyncio import QueueEmpty
 from callsmusic import callsmusic
 from callsmusic.queues import queues
 from config import BOT_USERNAME, que
-from Rockerz.cache.admins import admins
+from Gods.cache.admins import admins
 from helpers.channelmusic import get_chat_id
 from helpers.dbtools import delcmd_is_on, delcmd_off, delcmd_on, handle_user_status
 from helpers.decorators import authorized_users_only, errors
